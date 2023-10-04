bf10e6e4b6e78baaf0df26841d56f4c355918bcb
DSM-1 <dsm1@fatec.academico>
2023-10-03 22:44:56 -0300

    Atualização Site-Semantico

---
+++ site-semantico/main.js
@@ -0,0 +1,8 @@
+function Loading(){
+     document.querySelector("body").style.visibility="hidden";
+     document.getElementById("load").style.visibility="visible"
+    setTimeout(()=>{
+        document.querySelector("#load").style.visibility="hidden"
+        document.querySelector("body").style.visibility="visible";
+    },2500)
+}
